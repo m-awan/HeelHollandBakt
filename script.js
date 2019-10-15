822f1f551a8c7c5b01d2f67a0babb99c019bb37c
m-awan <azam_awan@Hotmail.com>
2019-10-16 00:04:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("burger").addEventListener("click", menuAnimatie);
+
+ function menuAnimatie() {
+
+
+ var element = document.getElementById("navigation");
+element.classList.toggle("active");
+link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.3}s`
+
+
+
+
+ 
+ }
