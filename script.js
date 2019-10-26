bba4f5f268a386e9628345369b13afffbc0877e0
m-awan <azam_awan@Hotmail.com>
2019-10-26 19:22:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 document.getElementById("burger").addEventListener("click", menuAnimatie);
 
- function menuAnimatie() {
+function menuAnimatie() {
 
 
- var element = document.getElementById("navigation");
-element.classList.toggle("active");
+    var element = document.getElementById("navigation");
+    element.classList.toggle("active");
 
- }
+}
 
 window.onscroll = () => {
-  const nav = document.querySelector('nav');
-  if(this.scrollY <= 10) nav.className = '' ; else nav.className = 'scroll';
+    const nav = document.querySelector('nav');
+    if (this.scrollY <= 10) nav.className = '';
+    else nav.className = 'scroll';
 };
 
 
-$('#taart').change(function(){
-    if($(this).is(":checked")) {
+$('#taart').change(function () {
+    if ($(this).is(":checked")) {
         $('.taart').removeClass('hidden');
     } else {
         $('.taart').addClass('hidden');
     }
 });
 
-$('#cake').change(function(){
-    if($(this).is(":checked")) {
+$('#cake').change(function () {
+    if ($(this).is(":checked")) {
         $('.cake').removeClass('hidden');
     } else {
         $('.cake').addClass('hidden');
     }
 });
-
-
-
-
-
-
